339946e14cc97870626339052c4c8695f216053c
phantomspecial <win.2000@live.jp>
2018-01-21 01:10:37 +0900

    Add-user-pages(index)1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where.not(id: current_user.id)
   end
 
   def show
@@ -10,10 +11,14 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = current_user
+    user.update(user_params)
+    redirect_to edit_user_path(current_user.id)
   end
 
   private
   def user_params
+    params.require(:user).permit(:email, :name, :birthday, :gender, :univ)
   end
 
 end
